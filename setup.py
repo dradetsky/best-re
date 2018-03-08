d6fe30ad871830361e02f5149677b701684bd9e0
Daniel Radetsky <dradetsky@gmail.com>
2018-03-08 08:28:19 -0800

    forgot to change copypasta

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='best-re',
     version='0.0.0',
-    description='keep the swagger dry',
+    description='import the best re lib',
     packages=find_packages('src'),
     package_dir={'': 'src'},
 )
